83831f5660ec2e4b7ce0362f64f57adff80b8db4
Daniel Haag <danni.haag91@gmail.com>
2021-12-09 18:37:25 -0300

    primeiro commit

---
+++ Params/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Params
+{
+    class Calculadora
+    {
+        public static int Sum(params int[] numeros)
+        {
+            int result = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                result += numeros[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Params/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculadora.Sum(5, 5, 5));
+            Console.Read();
+        }
+    }
+}
